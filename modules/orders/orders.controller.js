91c091a1c9164f9a0b3d8786745bd0676f45f56c
Fareed-Murtaza <fareed.murtaza@devsinc.com>
2022-08-17 16:37:38 -0700

    Add attributes in get all orders api

--- modules/orders/orders.controller.js
+++ modules/orders/orders.controller.js
@@ -1,5 +1,8 @@
 const db = require("../../models");
+
 const Orders = db.orders;
+const Products = db.products;
+const Inventory = db.inventory;
 
 const { Op } = require("sequelize");
 const { sequelize } = require("../../models");
@@ -7,7 +10,21 @@ const { sequelize } = require("../../models");
 // Find All Orders
 exports.findAll = (req, res) => {
     try {
-        Orders.findAll({})
+        Orders.findAll({
+            include: [
+                {
+                    model: Products,
+                    include: [
+                        {
+                            model: Inventory,
+                            attributes: ['color', 'size']
+                        }
+                    ],
+                    attributes: ['id', 'product_name']
+                }
+            ],
+            attributes: ['id', 'name', 'email', 'order_status', 'total_cents', 'transaction_id', 'shipper_name', 'tracking_number']
+        })
             .then(async orders => {
                 res.send(orders);
             })
