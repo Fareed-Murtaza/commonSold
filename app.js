4c453b0a5b68ffc08e30595a337cbee127731e83
Fareed-Murtaza <fareed.murtaza@devsinc.com>
2022-08-17 15:58:06 -0700

    Change port to 5000

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ class Server {
     }
 
     async appExecute() {
-        const port = process.env.PORT || 3000;
+        const port = process.env.PORT || 5000;
 
         this.appConfig();
         this.includeRoute();
