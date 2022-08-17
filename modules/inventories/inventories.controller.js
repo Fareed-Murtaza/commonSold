b65918095b8e8c9b7cb6bf7cad92c816be6f77dd
Fareed-Murtaza <fareed.murtaza@devsinc.com>
2022-08-17 16:03:48 -0700

    Add product join in get all inventory api

--- modules/inventories/inventories.controller.js
+++ modules/inventories/inventories.controller.js
@@ -2,6 +2,7 @@ const db = require("../../models");
 const Joi = require('@hapi/joi');
 
 const Inventory = db.inventory;
+const Products = db.products;
 
 const Op = db.Sequelize.Op;
 const { sequelize } = require("../../models");
@@ -9,7 +10,13 @@ const { sequelize } = require("../../models");
 // Find All Videos 
 exports.findAll = (req, res) => {
   try {
-    Inventory.findAll({})
+    Inventory.findAll({
+      include: [
+        {
+            model: Products
+        }
+      ],
+    })
       .then(data => {
         res.send(data);
       })
